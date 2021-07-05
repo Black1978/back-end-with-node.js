c66c38969403edbc3e2e41a823d4d2fec51894e4
Black1978 <chernyi.andrei@gmail.com>
2021-07-05 16:35:25 +0200

    Dependecies, create a databace connect, creating tables, routes

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+class UserController {
+    async registration (req, res) {
+        res.json('hiuuuu')
+    }
+    async login (req, res) {
+        
+    }
+    async check (req, res) {
+        
+    }
+}
+module.exports = new UserController ()
---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config() // Connect variebles
+const PORT = process.env.PORT || 5000
+
+const express = require('express') //Plug in express
+const cors = require('cors')
+const app = express()
+
+const sequelize = require('./db') //Connect the database with Sequelize
+const models = require('./models/models')//The tables
+
+const router = require('./routes/index')
+
+
+
+app.use(cors()) //The working with the requests
+app.use(express.json()) //The working with .json
+app.use('/api', router)
+
+const start = async () => {
+    try {
+        await sequelize.authenticate() //The database connect
+        await sequelize.sync() //The revise of database's state
+        app.listen(PORT, () => console.log(`The server started on a port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+ start()
+
